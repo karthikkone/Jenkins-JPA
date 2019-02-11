d49a0695ba5f86284f5691ff4cbef58f9304c146
karthikkone <karthik.andhiyur@kone.com>
2019-02-11 12:18:10 +0530

    Update BuildThread.java

--- src/main/java/com/jenkinsjobs/Jobs/BuildThread.java
+++ src/main/java/com/jenkinsjobs/Jobs/BuildThread.java
@@ -118,7 +118,7 @@ public class BuildThread implements Runnable
 	       //running = false;
 	       //interrupt();
 	       try {	       
-		
+		jenkins = new JenkinsServer(new URI("https://kone.iagilepro.com"), "agile.pro@kone.com", "infy1234");
 		while(queueItem == null)
 		{
 	           Thread.sleep(50L);
